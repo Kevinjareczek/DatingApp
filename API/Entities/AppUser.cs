ccfef6b59649c46a32e021e2b3cdadd857d69e9e
Kevin Jareczek <kevinjareczek@gmail.com>
2022-03-10 10:24:31 -0800

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; } //Name by convention for EF
+        public string UserName { get; set; } //Name by convention for EF
+
+    }
+}
